80cfd2e00b74c86ec796fb85fd2f5a6bbbb7482b
ofer <geon.arcan@gmail.com>
2025-02-01 22:00:03 +0200

    subdomains for nginx reverse proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {allowedHosts: ["raspberrypi"]},
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+  server: {
+    allowedHosts: ["raspberrypi"],
+    port: 5173,
+    strictPort: true
+  },
+  base: env.VITE_BASE_URL || '/',
   plugins: [react()],
-})
+}})
